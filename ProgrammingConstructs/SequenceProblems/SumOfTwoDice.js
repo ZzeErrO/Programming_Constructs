d50207b9447e69040b3e36d135dd21382015c1b0
prashikjaware <prashikjaware123@gmail.com>
2021-04-25 16:10:50 +0530

    [Prashik]Added: Suum of two dice numbers

---
+++ ProgrammingConstructs/SequenceProblems/SumOfTwoDice.js
@@ -0,0 +1,4 @@
+let value1 = Math.floor(Math.random() * 10) % 6 + 1;
+let value2 = Math.floor(Math.random() * 10) % 6 + 1;
+let result = value1 + value2;
+console.log("Addition of two random dice numbers "+ value1 +"+"+ value2 +"::= " + result);
