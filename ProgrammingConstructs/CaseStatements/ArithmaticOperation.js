2609be747794ff349559bdc12cd12666b156c0c3
prashikjaware <prashikjaware123@gmail.com>
2021-04-25 18:10:52 +0530

    [Prashik]Added: Minimum and Maximum of Aritmatic Operations

---
+++ ProgrammingConstructs/CaseStatements/ArithmaticOperation.js
@@ -0,0 +1,50 @@
+let a =(Math.floor(Math.random()*10)+1);
+let b =(Math.floor(Math.random()*10)+1);
+let c =(Math.floor(Math.random()*10)+1);
+console.log("values for A : "+a+" B : "+b+" C : "+c);
+
+let operation1 = Math.floor(a+b*c);
+let operation2 = Math.floor(a%b+c);
+let operation3 = Math.floor(c+a/b);
+let operation4 = Math.floor(a*b/c);
+
+console.log(" a+b*c = "+operation1);
+console.log(" a%b+c = "+operation2);
+console.log(" c+a/b = "+operation3);
+console.log(" a*b/c = "+operation4);
+
+switch(true){
+case (operation1<operation2 && operation1<operation3 && operation1 <operation4):
+    console.log(" minimum is "+operation1);
+    break
+case (operation2<operation1 && operation2<operation3 && operation2 <operation4):
+    console.log(" minimum is "+operation2);
+    break
+case (operation3<operation1 && operation3<operation2 && operation3 <operation4):
+    console.log(" minimum is "+operation3);
+    break
+case (operation4<operation1 && operation4<operation3 && operation4 <operation3):
+    console.log(" minimum is "+operation4);
+    break
+default:
+    console.log("Oops");
+    break
+}
+
+switch(true){
+case (operation1>operation2 && operation1>operation3 && operation1 >operation4):
+    console.log(" Maximum is "+operation1);
+    break
+case (operation2>operation1 && operation2>operation3 && operation2 >operation4):
+    console.log(" Maximum is "+operation2);
+    break
+case (operation3>operation1 && operation3>operation2 && operation3 >operation4):
+    console.log(" Maximum is "+operation3);
+    break
+case (operation4>operation1 && operation4>operation3 && operation4 >operation3):
+    console.log(" Maximum is "+operation4);
+    break
+default:
+    console.log("Oops")
+    break
+}
